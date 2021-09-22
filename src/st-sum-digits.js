7024f0b9390bb42d8e73bbfdd687fa7070f5ebc1
pashakiz <pashakiz@gmail.com>
2021-09-22 04:38:49 +0300

    feat: function getSumOfDigits

--- src/st-sum-digits.js
+++ src/st-sum-digits.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Given a number, replace this number with
  * the sum of its digits until we get to a one digit number.
@@ -12,7 +10,8 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-export default function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSumOfDigits(n) {
+  while(n.toString().length > 1)
+    n = n.toString().split('').reduce((acc, v) => +acc + +v)
+  return n
 }
