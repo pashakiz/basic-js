2b816abebc0c14839c50593c666deac0b6845721
pashakiz <pashakiz@gmail.com>
2021-10-04 23:53:24 +0300

    implement function renameFiles

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * There's a list of file, since two files cannot have equal names,
  * the one which comes later will have a suffix (k),
@@ -15,7 +13,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(names) {
+  let namesCounts = {}
+  return names.reduce((acc, name, i) => {
+    if (name in namesCounts) {
+      namesCounts[name] = namesCounts[name] + 1 || 1
+      acc.push(`${name}(${namesCounts[name]})`)
+    } else {
+      if (acc.includes(name)) {
+        namesCounts[`${name}(${1})`] = 0
+        acc.push(`${name}(${1})`)
+      } else {
+        namesCounts[name] = 0
+        acc.push(name)
+      }
+    }
+    return acc
+  }, [])
 }
